d94edc33c74fba414aecaf5619b17cf40988279b
KATO JUN <c241006@ivy.ac.jp>
2025-09-01 13:39:59 +0900

    お試し　トミカ！！！！

--- login.php
+++ login.php
@@ -12,11 +12,7 @@ if ($_SERVER["REQUEST_METHOD"]==="POST") {
     $username = $_POST["username"]??"";
     $password = $_POST["password"]??"";
 
-    $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
-    $stmt->execute([$username]);
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if ($user && password_verify($password, $user["password"])) {
+    if ($username === $valid_username && $password === $valid_password) {
         // セッションに保存して遷移
         $_SESSION["user_id"] = $user["id"];
         $_SESSION["username"] = $user["username"];
